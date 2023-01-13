99d6d1b9731a2b64fd0cc15c5fe445ff67115a34
agoodson582 <agoodson582@gmail.com>
2023-01-13 10:04:15 -0500

    Added mobile menu functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello world! Thanks for visiting!");
+const CLASS_HIDDEN = 'hidden'
+
+function toggleMobileLinks() {
+    let mLinks = document.querySelector('header .links.mobile-only')
+    console.log(mLinks.classList)
+    mLinks.classList.toggle(CLASS_HIDDEN)
+}
+
+document.getElementsByClassName('hamburger-menu')[0].addEventListener('click', () => {
+    toggleMobileLinks()
+})
+
+console.log("Hello world! Thanks for visiting!")
